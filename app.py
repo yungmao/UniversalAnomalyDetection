d2d1906853c11f5f816bd076cfe2adc2f89ab982
Daniel <daniel.jablonski1998@gmail.com>
2021-01-13 05:08:17 +0100

    First step

--- app.py
+++ app.py
@@ -1,31 +1,49 @@
-from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+# We'll render HTML templates and access data sent by POST
+# using the request object from flask. Redirect and url_for
+# will be used to redirect the user once the upload is done
+# and send_from_directory will help us to send/show on the
+# browser the file that the user just uploaded
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
 
+import os
+
+# Initialize the Flask application
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///filestorage.db'
-db = SQLAlchemy(app)
 
-class FileContents(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(300))
-    data = db.Column(db.LargeBinary)
+# This is the path to the upload directory
+app.config['UPLOAD_FOLDER'] = 'static/uploads/'
+# These are the extension that we are accepting to be uploaded
+app.config['ALLOWED_EXTENSIONS'] = set(['mat','csv','json'])
 
+# For a given file, return whether it's an allowed type or not
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
+# This route will show a form to perform an AJAX request
+# jQuery is loaded to execute the request and update the
+# value of the operation
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/upload', methods= ['GET', 'POST'])
+# Route that will process the file upload
+@app.route('/upload', methods=['POST'])
 def upload():
-    if request.method == 'POST':
-        if request.files:
-            file = request.files['inputFile']
-            newFile = FileContents(name=file.filename, data=file.read())
-            db.session.add(newFile)
-            db.session.commit()
-            return 'Saved' + file.filename + 'to the database'
-
-
+    file = request.files['file']
+    filename = secure_filename(file.filename)
+    if filename != '':
+        if file and allowed_file(filename):
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(
+        host="0.0.0.0",
+        port=int("80"),
+        debug=True
+    )
+
+
+
